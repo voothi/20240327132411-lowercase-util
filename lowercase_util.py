d82e3888697d8abefb271d77e26d2531515bcaab
Denis Novikov <voothi@gmail.com>
2024-03-27 13:34:14 +0100

    20240327133413

--- lowercase_util.py
+++ lowercase_util.py
@@ -1,14 +1,20 @@
 import argparse
+import pyperclip
 
 def to_lower_case(input_string):
     return input_string.lower()
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert input string to lowercase.")
-    parser.add_argument("input_string", type=str, help="Input string to convert to lowercase.")
+    parser = argparse.ArgumentParser(description="Convert input string or clipboard content to lowercase.")
+    parser.add_argument("input_string", nargs='?', type=str, help="Input string to convert to lowercase. If not provided, clipboard content will be used.")
     args = parser.parse_args()
 
-    output_string = to_lower_case(args.input_string)
+    if args.input_string is None:
+        clipboard_content = pyperclip.paste()
+        output_string = to_lower_case(clipboard_content)
+    else:
+        output_string = to_lower_case(args.input_string)
+
     print(output_string)
 
 if __name__ == "__main__":
